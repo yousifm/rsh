93f5c93981edbc9230741569a942543f6f2fea34
yousifm <ymahmoud1b@gmail.com>
2020-06-07 22:20:20 +0800

    Change prompt

--- src/lib/prompt/mod.rs
+++ src/lib/prompt/mod.rs
@@ -1,6 +1,13 @@
 use std::io::{stdout, Write};
+use std::env;
 
 pub fn print_prompt() {
-    print!("> ");
+    let user = env::var("USER").unwrap();
+    let mut pwd = env::var("PWD").unwrap();
+    let home = env::var("HOME").unwrap(); 
+
+    pwd = pwd.replace(home.as_str(), "~");
+
+    print!("[{} {}> ", user, pwd);
     stdout().flush().unwrap();
 }
