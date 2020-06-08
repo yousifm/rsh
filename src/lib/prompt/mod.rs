e0ab038b602615dabb2a16a72965d8e04ccd353e
yousifm <ymahmoud1b@gmail.com>
2020-06-08 16:32:55 +0800

    Change from ansiterm to termion

--- src/lib/prompt/mod.rs
+++ src/lib/prompt/mod.rs
@@ -1,18 +1,24 @@
 use std::env;
 use std::io::Write;
-use ansi_term::Color::{Green, Blue, };
+use termion::{color, style};
 
 pub fn print_prompt() {
     let user = env::var("USER").unwrap();
     let mut pwd = String::from(env::current_dir().unwrap().to_str().unwrap());
-    let home = env::var("HOME").unwrap(); 
+    let home = env::var("HOME").unwrap();
 
     pwd = pwd.replace(home.as_str(), "~");
 
-    print!("{}", Green.bold().paint("["));
-    print!("{} ", Green.bold().underline().paint(user));
-    print!("{}", Blue.bold().paint(pwd));
-    print!("{}", Green.bold().paint("> "));
-
+    print!(
+        "{}{}[{} {}{}{}> {}{}",
+        style::Bold,
+        color::Fg(color::Green),
+        user,
+        color::Fg(color::Blue),
+        pwd,
+        color::Fg(color::Green),
+        color::Fg(color::Reset),
+        style::Reset,
+    );
     std::io::stdout().flush().unwrap();
 }
